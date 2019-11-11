fc2b7ec30d4318b87ddb424f3f188dc1f04ffab4
Santiago MVO <santiago.mvao@gmail.com>
2019-11-10 21:14:53 -0600

    added array of words and function to select random word and reset number of guesses

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 var Word = require("./Letter");
 
+var engine = {
+    wordsArray:["Apple", "Orange", "Pineapple", "Pumpkin", "Banana", "Cherry", "Watermelon", "Strawberry",
+    "Grape", "Lime", "Lemon", "Pear", "Papaya", "Grapefruit", "Peach", "Apricot", "Tomato", "Avocado", "Guava"],
+
+    activeWord:"",
+
+    remainingGuesses:0,
+
+    maxGuesses:8,
+
+    newWord:function(){
+        this.activeWord = new Word(this.wordsArray[randomNumber(this.wordsArray.length-1,0)]);
+        this.remainingGuesses = this.maxGuesses;
+    }
+}
+
+function randomNumber (max=10, min=1){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
